a73af500a8fc9fd7d14c6e09ad7e552c7d008ddc
nikkkitosss <162501277+nikkkitosss@users.noreply.github.com>
2024-03-20 18:25:37 +0200

    in ex1.asm making arrays (key and value)

--- ex1.asm
+++ ex1.asm
@@ -2,62 +2,150 @@
 .stack 100h
 
 .data
-buffer db 256 dup(?)      ; буфер для зчитування рядка
-key db 17 dup(?)          ; місце для збереження ключа (макс 16 символів + 1 для завершувача рядка)
-value dw ?                ; змінна для збереження значення
-CR equ 0Dh                ; код символу CR
-LF equ 0Ah                ; код символу LF
+filename    db "V3TEST1.IN", 0
+mes db "Success $"
+mesBad db "File error $"
+handle dw 0
+oneChar db 0
+
+keys db 5000*16 dup(0)
+keyInd dw 0
+isWord db 1
+values db 5000*16 dup(0)
+valInd dw 0
+
+
 
 .code
 main proc
     mov ax, @data
     mov ds, ax
 
-    mov cx, 10000          ; максимальна кількість рядків
-read_loop:
-    ; зчитування рядка з stdin
-    lea dx, buffer
-    mov ah, 0Ah             ; зчитати рядок з stdin
-    int 21h
+mov dx, offset fileName; Address filename with ds:dx 
+mov ah, 03Dh ;DOS Open-File function number 
+mov  al, 0;  0 = Read-only access 
+int 21h; Call DOS to open file 
 
-    ; перевірка на EOF  
-    cmp buffer, '$'
-    je end_input
+jc error ;Call routine to handle errors
+    jmp cont
+ error:
+     mov ah, 09h
+ mov dx, offset mesBad
+int 21h
+jmp ending
 
-    ; пошук кінця рядка
-    mov si, offset buffer + 2 ; адресу початку рядка
-    mov cx, 255             ; обмежити пошук до максимальної довжини рядка
-    xor al, al
-    repne scasb             ; шукаємо перший не нульвий символ в al
-    mov cx, 16              ; максимальна довжина ключа
-    mov di, offset key      ; початок адреси для зберігання ключа 
-    mov al, ' '             ; роздільник між ключем та значенням
-    cld
-    repne scasb             ; шукаємо пробіл, що розділяє ключ та значення, за допомогоб порівнння з al 
-    je invalid_input        ; якщо пробіл не знайдено, це невірний формат введення
 
-    ; копіюємо ключ до key, включно з пробілом
-    mov cx, di
-    sub cx, offset key
-    mov al, 0               ; додаємо завершувач рядка
-    mov byte ptr [di], al
-    inc di                  ; перехід до наступного байту 
 
-    ; перевірка довжини ключа
-    cmp cx, 17
-    jge invalid_input       ; якщо довжина ключа перевищує 16 символів, ввід недійсний
+cont:
+mov [handle] , ax ; Save file handle(adress) for later
 
-    ;...
+;read file and put characters into buffer
+read_next:
+    mov ah, 3Fh
+    mov bx, [handle]  ; file handle
+    mov cx, 1   ; 1 byte to read
+    mov dx, offset oneChar   ; read to ds:dx 
+    int 21h   ;  ax = number of bytes read
+    ; do something with [oneChar]
 
-    jmp read_loop
+    ;save ax
+    push ax
+    push bx ; 
+    push cx ; 
+    push dx ; not necessary?
+    call procChar
+;return ax vaulue
+pop dx
+pop cx
+pop bx
+pop ax ; for reading 
+    or ax,ax  ;checking 
+    jnz read_next
+;clean values last number бо остання цифра в key записуеться двічі бо oneChar залишає останній символ в собі 
+        mov si, offset values
+        mov bx, valInd
+        dec bx
+        add si, bx
+        mov al, 0
+        mov [si], al
 
-end_input:
-    mov ax, 4C00h
-    int 21h
-
-invalid_input:
-    ; обробка недійсного вводу
-    jmp read_loop
-
+ mov ah, 09h
+ mov dx, offset mes
+int 21h
+ending:
 main endp
+
+
+
+procChar proc
+    ;pop dx; save adress to dx
+    ;mov saveV, dx
+    cmp oneChar,0Dh
+jnz notCR
+;change isWord to 1
+ mov isWord,1
+ ; go to next position in values
+ mov ax, valInd
+ mov cx, 16
+ ;div cx; now ax has number of values
+  shr ax, 4        ; Shift right by 4 position
+ inc ax; go to next value 
+mul cx; ax has position of next value
+mov valInd,ax
+  
+
+    jmp endProc
+notCR:
+cmp oneChar,0Ah
+jnz notLF
+;change isWord to 1
+mov isWord,1
+    jmp endProc
+notLF:
+cmp oneChar,20h
+jnz notSpace
+;chance isWord to 0
+mov isWord,0
+; go to next position in keys
+ mov ax, keyInd
+ mov cx, 16
+ ;div cx; now ax has number of keys
+ shr ax, 4 ; Shift right by 4 position ділення на 16 не спрацювало тому побітовий сув на 4 ( щоб запонвити поусті комірки в одному key)
+ inc ax; go to next key 
+mul cx; ax has position of next key
+mov keyInd,ax
+ 
+jmp endProc
+
+
+
+notSpace:
+    cmp isWord, 0 ; key or value 
+    jnz itsWord
+        ;save char to values
+        mov si, offset values
+       
+        mov bx, valInd
+        add si, bx
+        mov al, oneChar
+        mov [si], al
+        inc valInd
+          jmp endProc
+itsWord:
+    
+         ;save char to keys 
+        mov si, offset keys
+
+        mov bx, keyInd 
+        add si, bx
+        mov al, oneChar
+
+        mov [si], al
+        inc keyInd 
+      
+
+endProc:
+    ;push dx
+    ret
+procChar endp   
 end main
